4c3a64032282bcea9ce8c7e99e147519a6faf536
Nicholas Tinsley <00sturm@gmail.com>
2019-11-06 08:01:55 -0500

    formatted with yapf

--- double_speed_H264.py
+++ double_speed_H264.py
@@ -1,4 +1,5 @@
 #! python3
+''' Formatted with yapf'''
 
 import ffmpeg
 import sys
@@ -12,66 +13,93 @@ MAX_OUTPUT_FRAME_RATE = 60
 FILE_NAME_TEMPLATE = "%(uploader)s_%(title)s"
 SPEED_FACTOR = 2.0
 
+
 def get_height(filename):
-  try:
-    probe = ffmpeg.probe(filename)
-    video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-    height = int(video_stream['height'])
-    return height
-  except ffmpeg.Error as e:
-    print(e.stderr)
-    raise e
+    try:
+        probe = ffmpeg.probe(filename)
+        video_stream = next((stream for stream in probe['streams']
+                             if stream['codec_type'] == 'video'), None)
+        height = int(video_stream['height'])
+        return height
+    except ffmpeg.Error as e:
+        print(e.stderr)
+        raise e
+
 
 def get_frame_rate(filename):
-  probe = ffmpeg.probe(filename)
-  video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-  fps = eval(video_stream['r_frame_rate'])
-  return float(fps)
-  
+    probe = ffmpeg.probe(filename)
+    video_stream = next(
+        (stream
+         for stream in probe['streams'] if stream['codec_type'] == 'video'),
+        None)
+    fps = eval(video_stream['r_frame_rate'])
+    return float(fps)
+
+
 def main():
-  downloaded_videos = []
-  ydl_opts = {
-    'format': 'bestvideo[fps<=%(fps)s]+bestaudio/best' % {"fps": MAX_INPUT_FRAME_RATE},
-    'outtmpl': FILE_NAME_TEMPLATE,
-    'restrictfilenames': True,
-    'merge_output_format': 'mkv'
-  }
+    downloaded_videos = []
+    ydl_opts = {
+        'format': 'bestvideo[fps<=%(fps)s]+bestaudio/best' % {
+            "fps": MAX_INPUT_FRAME_RATE
+        },
+        'outtmpl': FILE_NAME_TEMPLATE,
+        'restrictfilenames': True,
+        'merge_output_format': 'mkv'
+    }
 
-  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    for url in sys.argv[1:]:
-      try:
-        extracted_info = ydl.extract_info(url)
-        if "_type" in extracted_info and "entries" in extracted_info and extracted_info["_type"] is 'playlist':
-          for entry in extracted_info["entries"]:
-            filename = ydl.prepare_filename(entry) + ".mkv"
-            if filename not in downloaded_videos:
-              downloaded_videos.append(filename)
-        else:
-          filename = ydl.prepare_filename(extracted_info) + ".mkv"
-          if filename not in downloaded_videos:
-            downloaded_videos.append(filename)
-      except:
-        print(f'failed to download {url}')
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        for url in sys.argv[1:]:
+            try:
+                extracted_info = ydl.extract_info(url)
+                if "_type" in extracted_info and "entries" in extracted_info and extracted_info[
+                        "_type"] is 'playlist':
+                    for entry in extracted_info["entries"]:
+                        filename = ydl.prepare_filename(entry) + ".mkv"
+                        if filename not in downloaded_videos:
+                            downloaded_videos.append(filename)
+                else:
+                    filename = ydl.prepare_filename(extracted_info) + ".mkv"
+                    if filename not in downloaded_videos:
+                        downloaded_videos.append(filename)
+            except:
+                print(f'failed to download {url}')
 
-  for in_file_name in downloaded_videos:
-    file_name_root = os.path.splitext(in_file_name)[0]
-    destination_file = file_name_root  + "_[%dx].mp4" % SPEED_FACTOR
-    if os.path.isfile(destination_file):
-      continue
+    for in_file_name in downloaded_videos:
+        file_name_root = os.path.splitext(in_file_name)[0]
+        destination_file = file_name_root + "_[%dx].mp4" % SPEED_FACTOR
+        if os.path.isfile(destination_file):
+            continue
 
-    new_height = get_height(in_file_name)
+        new_height = get_height(in_file_name)
 
-    inputObject = ffmpeg.input(in_file_name, vaapi_device='/dev/dri/renderD128')
-    v1 = inputObject['v'].setpts("PTS/%s" % SPEED_FACTOR).filter_(filter_name='hwupload')
-    if (new_height > MAX_HEIGHT):
-      v1 = v1.filter_('scale_vaapi', w = -2, h = MAX_WIDTH, format = 'nv12')
-    a1 = inputObject['a'].filter('atempo', SPEED_FACTOR)
+        inputObject = ffmpeg.input(in_file_name,
+                                   vaapi_device='/dev/dri/renderD128')
+        v1 = inputObject['v'].setpts(
+            "PTS/%s" % SPEED_FACTOR).filter_(filter_name='hwupload')
+        if (new_height > MAX_HEIGHT):
+            v1 = v1.filter_('scale_vaapi', w=-2, h=MAX_WIDTH, format='nv12')
+        a1 = inputObject['a'].filter('atempo', SPEED_FACTOR)
 
-    temp_file_name = file_name_root + ".tmp"
+        temp_file_name = file_name_root + ".tmp"
 
-    ffmpeg.output(v1, a1, temp_file_name, format='mp4', vcodec='h264_vaapi', video_bitrate="8M", gop_size = 240, acodec='aac', audio_bitrate="192k", r=min(SPEED_FACTOR*get_frame_rate(in_file_name), MAX_OUTPUT_FRAME_RATE)).global_args('-hide_banner').run(overwrite_output=True)
-    ffmpeg.input(temp_file_name).output(destination_file, codec='copy').global_args('-hide_banner').run(overwrite_output=True)
-    os.remove(temp_file_name)
+        ffmpeg.output(
+            v1,
+            a1,
+            temp_file_name,
+            format='mp4',
+            vcodec='h264_vaapi',
+            video_bitrate="8M",
+            gop_size=240,
+            acodec='aac',
+            audio_bitrate="192k",
+            r=min(SPEED_FACTOR * get_frame_rate(in_file_name),
+                  MAX_OUTPUT_FRAME_RATE)).global_args('-hide_banner').run(
+                      overwrite_output=True)
+        ffmpeg.input(temp_file_name).output(
+            destination_file, codec='copy').global_args('-hide_banner').run(
+                overwrite_output=True)
+        os.remove(temp_file_name)
 
-if __name__== "__main__":
-  main()
+
+if __name__ == "__main__":
+    main()
