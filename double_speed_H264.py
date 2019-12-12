8814360c5e3b5cd7fa9e7085535534aec0ac9107
Nicholas Tinsley <00sturm@gmail.com>
2019-12-12 08:25:35 -0500

    reintroduce force_original_aspect_ratio="decrease"

--- double_speed_H264.py
+++ double_speed_H264.py
@@ -102,7 +102,10 @@ def main():
                                    vaapi_device='/dev/dri/renderD128')
         v1 = inputObject['v'].setpts("PTS/%s" % SPEED_FACTOR)
         if (new_height > MAX_HEIGHT):
-            v1 = v1.filter('scale', -2, MAX_HEIGHT)
+            v1 = v1.filter('scale',
+                           -2,
+                           MAX_HEIGHT,
+                           force_original_aspect_ratio="decrease")
         v1 = v1.filter('format', 'nv12').filter_(filter_name='hwupload')
 
         a1 = inputObject['a'].filter('atempo', SPEED_FACTOR)
