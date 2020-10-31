3ce2c4c1157ab775df687b78bec06a5ce77aaa51
Nicholas Tinsley <00sturm@gmail.com>
2020-10-31 08:37:22 -0400

    fetch sponsorblock info
    actually applying the data is the next part

--- double_speed_HEVC.py
+++ double_speed_HEVC.py
@@ -2,8 +2,11 @@
 ''' Formatted with yapf '''
 
 import ffmpeg
+import json
 import sys
 import os
+from pprint import pprint
+import requests
 import youtube_dl
 from datetime import datetime
 
@@ -52,12 +55,15 @@ def download_videos(videos, opts, retries_remaining):
                         "_type"] == 'playlist':
                     for entry in extracted_info["entries"]:
                         filename = ydl.prepare_filename(entry) + ".mkv"
+                        # pprint(f'=== INFO FOR {filename} === ')
+                        # pprint(entry)
                         if filename not in result_list:
-                            result_list.append(filename)
+                            result_list.append((entry['display_id'], filename))
                 else:
                     filename = ydl.prepare_filename(extracted_info) + ".mkv"
                     if filename not in result_list:
-                        result_list.append(filename)
+                        result_list.append(
+                            (extracted_info['display_id'], filename))
             except KeyboardInterrupt:
                 print("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                 print("keyboard interrupt, aborting")
@@ -70,6 +76,14 @@ def download_videos(videos, opts, retries_remaining):
                 return download_videos(videos, opts, retries_remaining - 1)
 
     return result_list
+
+
+def fetch_sponsored_bits(video_id):
+    payload = f'videoID={video_id}&categories=["sponsor", "intro", "outro"]'
+    r = requests.get(f'https://sponsor.ajay.app/api/skipSegments',
+                     params=payload)
+    output = r.text
+    return output
 
 
 def main():
@@ -86,7 +100,21 @@ def main():
 
     encoded_video_count = 0
 
-    for in_file_name in downloaded_videos:
+    for display_id, in_file_name in downloaded_videos:
+        sponsored_segment_response = fetch_sponsored_bits(display_id)
+
+        if sponsored_segment_response == 'Not Found':
+            print("No sponsored segments for this one.")
+        else:
+            segments = json.loads(sponsored_segment_response)
+            for segment in segments:
+                segment_category = segment["category"]
+                segment_start = segment["segment"][0]
+                segment_end = segment["segment"][1]
+                print(
+                    f'Found {segment_category} block of length {segment_end - segment_start}'
+                )
+
         file_name_root = os.path.splitext(in_file_name)[0]
         destination_file = "{:.2f}x_".format(
             SPEED_FACTOR) + file_name_root + ".mp4"
@@ -97,12 +125,13 @@ def main():
         new_height = get_height(in_file_name)
 
         inputObject = ffmpeg.input(in_file_name)
+
         v1 = inputObject['v'].setpts("PTS/%s" % SPEED_FACTOR)
         if (new_height > MAX_HEIGHT):
             v1 = v1.filter('scale',
-                       -2,
-                       MAX_HEIGHT,
-                       force_original_aspect_ratio="decrease")
+                           -2,
+                           MAX_HEIGHT,
+                           force_original_aspect_ratio="decrease")
         a1 = inputObject['a'].filter('atempo', SPEED_FACTOR)
 
         temp_file_name = file_name_root + ".tmp"
